6a63a71c28c4a72024cd1e30c0a867c4dc041574
meiyi <meiyi@DESKTOP-759F2PU>
2026-01-13 14:44:45 +0800

    格式

--- TestWPf/ViewModels/PLCDeviceViewModel.cs
+++ TestWPf/ViewModels/PLCDeviceViewModel.cs
@@ -10,7 +10,7 @@ using System.Windows;
 
 namespace TestWPf.ViewModels
 {
-    public class PLCDeviceViewModel : ViewModelBase
+    public class  PLCDeviceViewModel : ViewModelBase
     {
 
         private PLCDevice _device;
